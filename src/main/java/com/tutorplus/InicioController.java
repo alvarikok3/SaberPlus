98ad1ca8aee5a731a875e18495d8b7b7e751d0e9
alvarikok3 <107597713+alvarikok3@users.noreply.github.com>
2025-06-16 15:57:18 -0600

    Diseño principal index con fondo azul y botones Signed-off-by:alvarikok3 <107597713+alvarikok3@users.noreply.github.com>

--- src/main/java/com/tutorplus/InicioController.java
+++ src/main/java/com/tutorplus/InicioController.java
@@ -6,16 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class InicioController {
-
     @GetMapping("/")
     public String inicio(Model model) {
         model.addAttribute("titulo", "Bienvenido a Tutor+");
         return "index";
-    }
-
-    @GetMapping("/nosotros")
-    public String nosotros(Model model) {
-        model.addAttribute("titulo", "Sobre Nosotros");
-        return "nosotros";
     }
 }
